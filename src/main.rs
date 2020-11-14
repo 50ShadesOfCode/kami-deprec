c6e6eee5577e4d6ebeb6ec625b3769c1b26a23ed
50ShadesOfCode <kaktysh20040130@gmail.com>
2020-11-14 18:42:03 +0300

    intial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate clap;
+use clap::{crate_version, App};
+
+fn main() {
+    let matches = App::new("kami")
+        .version(crate_version!())
+        .about("This is about living")
+        .author("50ShadesOfCode <kaktysh20040130@gmail.com>")
+        .get_matches();
+    println!("Ok")
+}
