5a4f357ac30af150ad3c489942fc3f782eb96d23
50ShadesOfCode <48156891+50ShadesOfCode@users.noreply.github.com>
2022-03-29 15:15:29 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use clap::{crate_version, App};
 fn main() {
     let matches = App::new("kami")
         .version(crate_version!())
-        .about("This is about living")
+        .about("This is about coding")
         .author("50ShadesOfCode <kaktysh20040130@gmail.com>")
         .get_matches();
-    println!("Okkk release")
+    println!("..")
 }
